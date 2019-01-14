b92b5b8398fe8d459ee7706609a741e2db4b9eca
Sam Kerrison <sam.kerrison@thedropbears.org.au>
2019-01-14 16:59:15 +1100

    added file

---
+++ utilities/navx.py
@@ -0,0 +1,27 @@
+import math
+
+from robotpy_ext.common_drivers.navx.ahrs import AHRS
+
+from .imu import IMU
+
+
+class NavX(IMU):
+    """Wrapper around RobotPy NavX to be compatible with our BNO055 driver."""
+
+    def __init__(self):
+        self.ahrs = AHRS.create_spi(update_rate_hz=50)
+
+    def getAngle(self) -> float:
+        """Get NavX angle.
+        Returns:
+            Angle in radians between -pi and +pi.
+        """
+        raw = self.ahrs.getYaw()
+        return -math.radians(raw)
+
+    def resetHeading(self):
+        self.ahrs.reset()
+
+    def getHeadingRate(self):
+        # multiply by 100 because NavX does not normalise per timestep
+        return math.radians(-self.ahrs.getRate()) * 50
