8126c2a304013b87061f33c81dec60f3535cfb02
Sam Kerrison <sam.kerrison@thedropbears.org.au>
2019-01-14 16:56:11 +1100

    added the getangle form the navx

--- autonomous/autonomous.py
+++ autonomous/autonomous.py
@@ -17,30 +17,30 @@ class LeftStartAuto(AutonomousStateMachine):
 
 	
 	def __init__(self):
-		self.lb_stage = 1
-		self.lb_x_1 = 0
-		self.lb_y_1 = -2
-		self.lb_spin_1 = math.pi 
-		self.lb_x_2 = -3.25
-		self.lb_y_2 = -1.2
-		self.lb_spin_2 = (math.pi/2)*-1
-		self.lb_x_3 = -3.5
-		self.lb_y_3 = -1.2
-		self.lb_spin_3 = (math.pi/2)*-1
-		self.lb_x_4 = -3.75
-		self.lb_y_4 = -1.2
-		self.lb_spin_4 = (math.pi/2)*-1
+		self.loading_bay_stage = 1
+		self.loading_bay_x_1 = 0
+		self.loading_bay_y_1 = -2
+		self.loading_bay_spin_1 = math.pi 
+		self.loading_bay_x_2 = -3.25
+		self.loading_bay_y_2 = -1.2
+		self.loading_bay_spin_2 = (math.pi/2)*-1
+		self.loading_bay_x_3 = -3.5
+		self.loading_bay_y_3 = -1.2
+		self.loading_bay_spin_3 = (math.pi/2)*-1
+		self.loading_bay_x_4 = -3.75
+		self.loading_bay_y_4 = -1.2
+		self.loading_bay_spin_4 = (math.pi/2)*-1
 
-		self.scb_stage = 1
-		self.scb_x_1 = 3.25
-		self.scb_y_1 = 1.2
-		self.scb_spin_1 = math.pi/2
-		self.scb_x_2 = 3.5
-		self.scb_y_2 = 1.2
-		self.scb_spin_2 = math.pi/2
-		self.scb_x_3 = 3.75
-		self.scb_y_3 = 1.2
-		self.scb_spin_3 = math.pi/2
+		self.side_cargoship_stage = 1
+		self.side_cargoship_x_1 = 3.25
+		self.side_cargoship_y_1 = 1.2
+		self.side_cargoship_spin_1 = math.pi/2
+		self.side_cargoship_x_2 = 3.5
+		self.side_cargoship_y_2 = 1.2
+		self.side_cargoship_spin_2 = math.pi/2
+		self.side_cargoship_x_3 = 3.75
+		self.side_cargoship_y_3 = 1.2
+		self.side_cargoship_spin_3 = math.pi/2
 
 
 
@@ -51,129 +51,53 @@ class LeftStartAuto(AutonomousStateMachine):
 			if self.chassis.odometry_x > 2.9:
 				self.chassis.set_inputs(0, 0, 0)
 				self.next_state_now("align_for_deployment")
-	
-	@state
-	def drive_to_loading_bay(self):
-		#first pickup
-		if self.lb_stage == 1:
-			self.chassis.set_inputs(0,self.lb_y_1,self.lb_spin_1)
-			if #facing loading bay
-				self.chassis.set_inputs(self.lb_y,self.lb_y_1,0)
-				self.lb_spin_1 = 0
-			if self.chassis.odometry_y < -2:
-				self.chassis.set_inputs(self.lb_x_1,0,self.lb_spin_1)
-				self.lb_y_1 = 0
-			if self.lb_y_1 == 0 and self.lb_spin_1 == 0:
-				self.chassis.set_inputs(-4,-0.6,0)
-				if self.chassis.odometry_x < 5.6:
-					self.chassis.set_inputs(0,0,0)
-					self.lb_stage +=1
-					self.next_state_now("align_for_intake")
-
-		elif self.lb_stage == 2:
-			self.chassis.set_inputs(self.lb_x_2,self.lb_y_2,self.lb_spin_2)
-			if #facing loading bay
-				self.chassis.set_input(self.lb_x_2,self.lb_y_2,0)
-				self.lb_spin_2 = 0
-			if self.chassis.odometry_x < - 6.5:
-				self.chassis.set_inputs(0,self.lb_y_2,self.lb_spin_2)
-				self.lb_x_2 = 0
-			if self.chassis.odometry_y < -2.4:
-				self.chass.set_inputs(self.lb_x_2,0,self.lb_spin_2)
-				self.lb_y_2 = 0
-			if self.lb_x_2 == 0 and self.lb_y_2 == 0 and lb_spin_2 == 0
-				self.lb_stage+=1
-				self.next_state_now("align_for_intake")
-
-
-		elif self.lb_stage == 3:
-			self.chassis.set_inputs(self.lb_x_3,self.lb_y_3,self.lb_spin_3)
-			if #facing loading bay
-				self.chassis.set_inputs(self._x_3,self.lb_y_3,0)
-				self.lb_spin_3 = 0
-			if self.chassis.odometry_x < - 6.5:
-				self.chassis.set_inputs(0,self.lb_y_3,self.lb_spin_3)
-				self.lb_x_3 = 0
-			if self.chassis.odometry_y < -2.4:
-				self.chass.set_inputs(self.lb_x_3,0,self.lb_spin_3)
-				self.lb_y_3 = 0
-			if self.lb_x_3 == 0 and self.lb_y_3 == 0 and self.lb_spin_3 == 0
-				self.lb_stage+=1
-				self.next_state_now("align_for_intake")
-
-
-		elif self.lb_stage == 4:
-			self.chassis.set_inputs(self.lb_x_4,self.lb_y_4,self.lb_spin_4)
-			if #facing loading bay
-				self.chassis.set_inputs(self.lb_x_4,self.lb_y_4,0)
-				self.lb_spin_4 = 0
-			if self.chassis.odometry_x < - 6.5:
-				self.chassis.set_inputs(0,self.lb_y_4,self.lb_spin_4)
-				self.lb_x_4 = 0
-			if self.chassis.odometry_y < -2.4:
-				self.chass.set_inputs(self.lb_x_4,0,spin)
-				self.lb_y_4 = 0
-			if self.lb_x_4 == 0 and self.lb_y_4 == 0 and self.lb_spin_4 == 0
-				self.lb_stage+=1
-				self.next_state_now("align_for_intake")
-
-
-
-
-
-
-
-			#getting back from side bays 
-		#if near loading bay
-			#self.next_state_now("align_for_intake")
-	pass
 
 	@state
 	def drive_to_side_cargo_bay(self):
 		#first pass
-		if self.scb_stage == 1:
-			self.chassis.set_inputs(self.scb_x_1,self.scb_y_1,self.scb_spin_1)
-			if #facing cargo bay
-				self.chassis.set_inputs(self.scb_x_1,self.scb_y_1,0)
-				self.scb_spin_1 = 0
+		if self.side_cargoship_stage == 1:
+			self.chassis.set_inputs(self.side_cargoship_x_1,self.side_cargoship_y_1,self.side_cargoship_spin_1)
+			if self.navx.getAngle() >= math.pi*1.5: #might be wrong depending on wat side the hatch is on 
+				self.chassis.set_inputs(self.side_cargoship_x_1,self.side_cargoship_y_1,0)
+				self.side_cargoship_spin_1 = 0
 			if self.chassis.odometry_x > 6.5:
-				self.chassis.set_inputs(0,self.scb_y_1,self.scb_spin_1)
-				self.scb_x_1 = 0
+				self.chassis.set_inputs(0,self.side_cargoship_y_1,self.side_cargoship_spin_1)
+				self.side_cargoship_x_1 = 0
 			if self.chassis.odometry_y > 2.4:
-				self.chassis.set_inputs(self.scb_x_1,0,self.scb_spin_1)
-				self.scb_y_1 = 0
-			if self.scb_x_1 == 0 and self.scb_y_1 == 0 and self.scb_spin_1 == 0:
-				self.scb_stage+=1
+				self.chassis.set_inputs(self.side_cargoship_x_1,0,self.side_cargoship_spin_1)
+				self.side_cargoship_y_1 = 0
+			if self.side_cargoship_x_1 == 0 and self.side_cargoship_y_1 == 0 and self.side_cargoship_spin_1 == 0:
+				self.side_cargoship_stage+=1
 				self.next_state_now("align_for_deployment")
 		#second pass
-		if self.scb_stage == 2:
-			self.chassis.set_inputs(self.scb_x_2,self.scb_y_2,self.scb_spin_2)
-			if #facing cargo bay
-				self.chassis.set_inputs(self.scb_x_2,self.scb_y_2,0)
-				self.scb_spin_2 = 0
+		if self.side_cargoship_stage == 2:
+			self.chassis.set_inputs(self.side_cargoship_x_2,self.side_cargoship_y_2,self.side_cargoship_spin_2)
+			if self.navx.getAngle() >= math.pi*1.5: #might be wrong depending on wat side the hatch is on 
+				self.chassis.set_inputs(self.side_cargoship_x_2,self.side_cargoship_y_2,0)
+				self.side_cargoship_spin_2 = 0
 			if self.chassis.odometry_x > 6.5:
-				self.chassis.set_inputs(0,self.scb_y_2,self.scb_spin_2)
-				self.scb_x_2 = 0
+				self.chassis.set_inputs(0,self.side_cargoship_y_2,self.side_cargoship_spin_2)
+				self.side_cargoship_x_2 = 0
 			if self.chassis.odometry_y > 2.4:
-				self.chassis.set_inputs(self.scb_x_2,0,self.scb_spin_2)
-				self.scb_y_2 = 0
-			if self.scb_x_2 == 0 and self.scb_y_2 == 0 and self.scb_spin_2 == 0:
-				self.scb_stage+=1
+				self.chassis.set_inputs(self.side_cargoship_x_2,0,self.side_cargoship_spin_2)
+				self.side_cargoship_y_2 = 0
+			if self.side_cargoship_x_2 == 0 and self.side_cargoship_y_2 == 0 and self.side_cargoship_spin_2 == 0:
+				self.side_cargoship_stage+=1
 				self.next_state_now("align_for_deployment")
 		#third pass
-		if self.scb_stage == 3:
-			self.chassis.set_inputs(self.scb_x_3,self.scb_y_3,self.scb_spin_3)
-			if #facing cargo bay
-				self.chassis.set_inputs(self.scb_x_3,self.scb_y_3,0)
-				self.scb_spin_3 = 0
+		if self.side_cargoship_stage == 3:
+			self.chassis.set_inputs(self.side_cargoship_x_3,self.side_cargoship_y_3,self.side_cargoship_spin_3)
+			if self.navx.getAngle() >= math.pi*1.5: #might be wrong depending on wat side the hatch is on 
+				self.chassis.set_inputs(self.side_cargoship_x_3,self.side_cargoship_y_3,0)
+				self.side_cargoship_spin_3 = 0
 			if self.chassis.odometry_x > 6.5:
-				self.chassis.set_inputs(0,self.scb_y_3,self.scb_spin_3)
-				self.scb_x_3 = 0
+				self.chassis.set_inputs(0,self.side_cargoship_y_3,self.side_cargoship_spin_3)
+				self.side_cargoship_x_3 = 0
 			if self.chassis.odometry_y > 2.4:
-				self.chassis.set_inputs(self.scb_x_3,0,self.scb_spin_3)
-				self.scb_y_3 = 0
-			if self.scb_x_3 == 0 and self.scb_y_3 == 0 and self.scb_spin_3 == 0:
-				self.scb_stage+=1
+				self.chassis.set_inputs(self.side_cargoship_x_3,0,self.side_cargoship_spin_3)
+				self.side_cargoship_y_3 = 0
+			if self.side_cargoship_x_3 == 0 and self.side_cargoship_y_3 == 0 and self.side_cargoship_spin_3 == 0:
+				self.side_cargoship_stage+=1
 				self.next_state_now("align_for_deployment")
 			
 
@@ -183,7 +107,6 @@ class LeftStartAuto(AutonomousStateMachine):
 
 		#if near side cargo bay without hatch
 			#self.next_state_now("align_for_deployment")
-		
 
 	@state
 	def align_for_deployment(self):
@@ -192,18 +115,81 @@ class LeftStartAuto(AutonomousStateMachine):
 		pass
 
 	@state
+	def deposit_hatch(self):
+		#if hatch deposited 
+			#self.next_state_now("drive_to_loading_bay")
+
+	@state
+	def drive_to_loading_bay(self):
+		#first pickup
+		if self.loading_bay_stage == 1:
+			self.chassis.set_inputs(0,self.loading_bay_y_1,self.loading_bay_spin_1)
+			if self.navx.getAngle() >= math.pi: #might be wrong depending on wat side the hatch is on 
+				self.chassis.set_inputs(self.loading_bay_y,self.loading_bay_y_1,0)
+				self.loading_bay_spin_1 = 0
+			if self.chassis.odometry_y < -2:
+				self.chassis.set_inputs(self.loading_bay_x_1,0,self.loading_bay_spin_1)
+				self.loading_bay_y_1 = 0
+			if self.loading_bay_y_1 == 0 and self.loading_bay_spin_1 == 0:
+				self.chassis.set_inputs(-4,-0.6,0)
+				if self.chassis.odometry_x < 5.6:
+					self.chassis.set_inputs(0,0,0)
+					self.loading_bay_stage +=1
+					self.next_state_now("align_for_intake")
+		#sencond pickup
+		elif self.loading_bay_stage == 2:
+			self.chassis.set_inputs(self.loading_bay_x_2,self.loading_bay_y_2,self.loading_bay_spin_2)
+			if self.navx.getAngle() >= math.pi: #might be wrong depending on wat side the hatch is on 
+				self.chassis.set_input(self.loading_bay_x_2,self.loading_bay_y_2,0)
+				self.loading_bay_spin_2 = 0
+			if self.chassis.odometry_x < - 6.5:
+				self.chassis.set_inputs(0,self.loading_bay_y_2,self.loading_bay_spin_2)
+				self.loading_bay_x_2 = 0
+			if self.chassis.odometry_y < -2.4:
+				self.chass.set_inputs(self.loading_bay_x_2,0,self.loading_bay_spin_2)
+				self.loading_bay_y_2 = 0
+			if self.loading_bay_x_2 == 0 and self.loading_bay_y_2 == 0 and loading_bay_spin_2 == 0
+				self.loading_bay_stage+=1
+				self.next_state_now("align_for_intake")
+
+		#third pickup
+		elif self.loading_bay_stage == 3:
+			self.chassis.set_inputs(self.loading_bay_x_3,self.loading_bay_y_3,self.loading_bay_spin_3)
+			if self.navx.getAngle() >= math.pi: #might be wrong depending on wat side the hatch is on 
+				self.chassis.set_inputs(self._x_3,self.loading_bay_y_3,0)
+				self.loading_bay_spin_3 = 0
+			if self.chassis.odometry_x < - 6.5:
+				self.chassis.set_inputs(0,self.loading_bay_y_3,self.loading_bay_spin_3)
+				self.loading_bay_x_3 = 0
+			if self.chassis.odometry_y < -2.4:
+				self.chass.set_inputs(self.loading_bay_x_3,0,self.loading_bay_spin_3)
+				self.loading_bay_y_3 = 0
+			if self.loading_bay_x_3 == 0 and self.loading_bay_y_3 == 0 and self.loading_bay_spin_3 == 0
+				self.loading_bay_stage+=1
+				self.next_state_now("align_for_intake")
+
+		#fourth pickup
+		elif self.loading_bay_stage == 4:
+			self.chassis.set_inputs(self.loading_bay_x_4,self.loading_bay_y_4,self.loading_bay_spin_4)
+			if self.navx.getAngle() >= math.pi: #might be wrong depending on wat side the hatch is on 
+				self.chassis.set_inputs(self.loading_bay_x_4,self.loading_bay_y_4,0)
+				self.loading_bay_spin_4 = 0
+			if self.chassis.odometry_x < - 6.5:
+				self.chassis.set_inputs(0,self.loading_bay_y_4,self.loading_bay_spin_4)
+				self.loading_bay_x_4 = 0
+			if self.chassis.odometry_y < -2.4:
+				self.chass.set_inputs(self.loading_bay_x_4,0,spin)
+				self.loading_bay_y_4 = 0
+			if self.loading_bay_x_4 == 0 and self.loading_bay_y_4 == 0 and self.loading_bay_spin_4 == 0
+				self.loading_bay_stage+=1
+				self.next_state_now("align_for_intake")
+		
+
+	@state
 	def align_for_intake(self):
 		#if aligned
 			#self.next_state_now("intake hatch")
 		pass
-
-
-	@state
-	def deposit_hatch(self):
-		#if hatch deposited 
-			#self.next_state_now("drive_to_loading_bay")
-		pass
-
 
 	@state
 	def intake_hatch(self):
