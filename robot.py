ee0f96b85b45a79ef719abf1257cc8c8c4c15388
michael <michael.kennedy@thedropbears.org.au>
2019-01-18 13:55:46 +1100

    Add pyswervedrive, navx and other dependecies

--- robot.py
+++ robot.py
@@ -2,13 +2,45 @@
 
 import magicbot
 import wpilib
+import ctre
+
+from automations.alignment import Aligner
 from components.vision import Vision
+from pyswervedrive.swervechassis import SwerveChassis
+from pyswervedrive.swervemodule import SwerveModule
+from utilities.navx import NavX
 
 
 class Robot(magicbot.MagicRobot):
+    aligner: Aligner
+
+    chassis: SwerveChassis
+
+    module_drive_free_speed: float = 84000.0  # encoder ticks / 100 ms
+
     def createObjects(self):
         """Create motors and stuff here."""
+        self.module_a = SwerveModule(
+            "a",
+            steer_talon=ctre.TalonSRX(42),
+            drive_talon=ctre.TalonSRX(48),
+            x_pos=-(0.375-0.160),
+            y_pos=0.375-0.100,
+            drive_free_speed=Robot.module_drive_free_speed,
+            reverse_steer_encoder=True,
+        )
+        self.module_b = SwerveModule(
+            "b",
+            steer_talon=ctre.TalonSRX(58),
+            drive_talon=ctre.TalonSRX(2),
+            x_pos=0.375-0.160,
+            y_pos=-(0.375-0.100),
+            drive_free_speed=Robot.module_drive_free_speed,
+            reverse_steer_encoder=True,
+        )
+
         self.vision = Vision()
+        self.imu = NavX()
 
     def teleopInit(self):
         """Initialise driver control."""
