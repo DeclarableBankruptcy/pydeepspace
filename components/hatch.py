04c35f923ac2a13f418c19d3b138241220b1628f
michael <michael.kennedy@thedropbears.org.au>
2019-01-29 10:23:10 +1100

    Divide the deposit pistons so the lower one can be fired before the others

--- components/hatch.py
+++ components/hatch.py
@@ -11,19 +11,23 @@ class Hatch:
     right_limit_switch: wpilib.DigitalInput
 
     def __init__(self):
-        self.punch_on = False
+        self.punch_on_top = False
+        self.punch_on_bottom = False
 
     def execute(self):
         """Run at the end of every control loop iteration."""
-        self.top_puncher.set(self.punch_on)
-        self.left_puncher.set(self.punch_on)
-        self.right_puncher.set(self.punch_on)
+        self.top_puncher.set(self.punch_on_bottom)
+        self.left_puncher.set(self.punch_on_top)
+        self.right_puncher.set(self.punch_on_top)
 
-    def punch(self):
-        self.punch_on = True
+    def punch_top(self):
+        self.punch_on_top = True
+
+    def punch_bottom(self):
+        self.punch_on_bottom = True
 
     def retract(self):
-        self.punch_on = False
+        self.punch_on_top = self.punch_on_bottom = False
 
     def contained(self):
         return any(
